439959d547741d104e687517a8902ef899341546
prabhakarkale <prabhakar.kale@cuelogic.com>
2021-06-01 13:36:50 +0530

    added MultiplyOperation.java file

---
+++ MultiplyOperation.java
@@ -0,0 +1,12 @@
+class MultiplyOperation
+{
+    Public Static void Main(String args[])
+    {
+        int i = 10, j = 10;
+        System.out.println("Multification : "+multiply(i,j));
+    }
+    int multiply(int a, int b)
+    {
+        return (a * b);
+    }
+}
