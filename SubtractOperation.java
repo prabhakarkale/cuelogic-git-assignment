162aa76099c4390d035e80ba51e61b3896338a64
prabhakarkale <prabhakar.kale@cuelogic.com>
2021-06-01 13:31:49 +0530

    added SubtractOperation.java file

---
+++ SubtractOperation.java
@@ -0,0 +1,12 @@
+class SubtractOperation
+{
+    Public Static void Main(String args[])
+    {
+        int i = 10, j = 10;
+        System.out.println("Subtraction : "+subtract(i,j));
+    }
+    int subtract(int a, int b)
+    {
+        return (a - b);
+    }
+}
